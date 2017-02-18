24524b6baef3cbf56291a44d7ec98584be8cd9d6
Mike (Lazlo) Vavro <mvavro@uci.edu>
2017-02-17 17:59:42 -0800

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void allocate(int newSize){
+	
+}
+
+void free(int blockNum){
+	
+}
+
+void blocklist(){
+	
+}
+
+
+int main(int argc, char* argv[]){
+	int blockNumber = 0;
+
+
+}
