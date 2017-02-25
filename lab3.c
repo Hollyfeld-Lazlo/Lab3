cc6a79f6086441aa706f9e5a55d4d21156877615
Lazlo <mvavro@uci.edu>
2017-02-24 16:48:06 -0800

    added free and fixed allocate

--- lab3.c
+++ lab3.c
@@ -10,66 +10,103 @@ int blockId;
 
 
 void allocate(int newSize){
-	char *p = head;
-	char *end = (p+127);
-			printf("newsize: %d \n", newSize);
-			printf("address of p : %d , address of end: %d , %d \n", p, end, *p);
+	unsigned char *p;
+	p = head;
+	unsigned char *end = (p+127);
+	//		printf("newsize: %d, *(p+1): %d \n", newSize, *(p+1));
+	//		printf("address of p : %d , address of end: %d , p: %d, *p+1: %d \n", p, end, *p, 128+(*(p+1)>>1) );
 	if(newSize > 126){
 		printf("Block Size Too Large..\n");
 		return;
 	}
-	while( (p < end) && ((*(p+1) & 1) || (*(p+1) & -2 <= (newSize+1))) ){
-			printf("Allocate: while: %d, size while: %d  \n",(*(p+1) & 1), (*(p+1) & -2));
-		p = p + 2 + (*(p+1) & -2);
+	if(newSize < 1){
+		printf("Invalid Block Size: must be greater than 0...\n");
 	}
-	printf("end: %d, p: %d, end-p: %d, newSize+1: %d\n", end, p, end-p, newSize+1);
+	//printf("Allocate::  p: %d, *p: %d, allocated: %d, size: %d\n",p, *p, (*(p+1) & 1), ((*(p+1)& -2)>>1));
+	//while( (p < end) && ((*(p+1) & 1) || (*(p+1) & -2 <= (newSize+1))) ){
+	// || ((((*(p+1) & -2) >> 1) - newSize) < 3)
+	while( (p < end) && ( (*(p+1) & 1) || (((*(p+1)&-2)>>1) < (newSize)) ) ){
+			//printf("Allocate: while: allocated: %d, Allocate: while: size: %d  \n",
+							//(*(p+1) & 1), (((*(p+1)&-2)>>1)));
+		p = p + 2 + ( (*(p+1) & -2) >> 1 );
+			//printf("Allocate: while: allocated: %d, Allocate: while: size: %d  \n",
+							//(*(p+1) & 1), (((*(p+1)&-2)>>1)));
+	}//you've found a suitable block							
+	
+	int padding = 0;
+	if((((*(p+1) & -2) >> 1) - newSize) < 3){
+		padding = (((*(p+1) & -2) >> 1) - newSize);
+		newSize = newSize + padding;
+	}
+	//printf("end: %d, p: %d, end-p: %d, newSize+1: %d\n", end, p, end-p, newSize+1);
 	if( (end-p-1) < newSize ){
 		printf("Not Enough Room...\n");
-	}
-	if(p >= head+126){
-		printf("Heap is Full...\n");
-		return;
-	}
-	int newBlockSize = newSize +1;
-	int oldSize = *(p+1) & -2;
-	*(p+1) = newBlockSize | 1;
-	if (newBlockSize < oldSize){
-		*(p+newBlockSize+2) = ++blockId;
-		*(p+newBlockSize+3) = oldSize - newBlockSize - 2;
-	}
+	}//new block size is bigger than all available spots
 
-	printf("newBlockId: %d, newBlockSize: %d, oldSize: %d, : %d  \n", *(p+newBlockSize+2), newBlockSize, oldSize, *(p+newBlockSize+1));
+	if(p >= head+126){
+		printf("End of Heap...\n");
+		return;
+	}//heap is full
+
+	int newBlockSize = (newSize << 1);
+	int oldSize = (*(p+1) & -2) >> 1;
+
+	if (newSize < oldSize){
+		*p = blockId++;				
+               	//printf("blockid: %d\n", blockId);	
+               	*(p+1) = newBlockSize | 1;				
+		*(p+newSize+2) = 0;
+		*(p+newSize+3) = ((oldSize - newSize - 2)<<1);
+		printf("%d %d\n", blockId, *p);
+	}
+	else{
+		*p = blockId++;				
+               	//printf("blockid: %d\n", blockId);
+               	*(p+1) = newBlockSize | 1;		
+		printf("%d %d\n", blockId,*p);
+	}
+	//printf("Allocate Success:: newBlockId: %d, newSize: %d, oldSize: %d, : %d  \n", *(p+newSize+2), newSize, oldSize, *(p+newBlockSize+1));
+	//printf("Allocate Success:: newBlockSize: %d, *P+newSize+3: %d \n", *(p+newSize+2), *(p+newSize+3));
 }
 
 
-void freeBlock(int blockNum){
-	
+void freeBlock(int targetBlock){	
+	unsigned char *p = head;
+	unsigned char *end = (p+127);
+	while((p < end) && (*p != targetBlock)){
+       		printf("block id: %d \n", *p);
+       		p = p + 2 + ((*(p+1) & -2) >> 1);
+	}
+	*(p+1) = *(p+1) & -2;
 }
 
 
 void blockList(){
-	char *p = head;
-	char *end = (p+127);
+	unsigned char *p = head;
+	unsigned char *end = (p+127);
 	char *booleanString[2];
 	booleanString[0] = "no";
 	booleanString[1] = "yes";
 	printf("Size\tAllocated\tStart\t\t\tEnd\n");
 	while(p < end){
 		printf("%d\t%s\t\t0x%08x\t\t0x%08x \n", 
-	 	(*(p+1) & -2), booleanString[(*(p+1) & 1)], p, (p + (*(p+1) & -2) + 1));
-		p = p + ((*(p+1) & -2) + 2);
+	 	//(*(p+1) & -2), booleanString[(*(p+1) & 1)], p, (p + (*(p+1) & -2) + 1));
+	 	((*(p+1) & -2)>>1)+2, booleanString[(*(p+1) & 1)], p, (p + ((*(p+1)& -2)>>1) + 1));
+		//p = p + ((*(p+1) & -2) + 2);
+		p = p + 2 + ((*(p+1)& -2)>>1);
 	}
 }
 
 
 void writeHeap(int targetBlock, char payload, int mult){
-	char *p = head;
-	char *end = (p+127);
+	unsigned char *p = head;
+	unsigned char *end = (p+127);
 	int i;
-	printf("writeheap :: payload: %c, mult: %d \n", payload, mult);
+	//printf("writeheap :: payload: %c, mult: %d \n", payload, mult);
 	while((p < end) && (*p != targetBlock)){
 		printf("block id: %d \n", *p);
-		p = p + 2 + (*(p+1) & -2);
+		//p = p + 2 + (*(p+1) & -2);
+		p = p + 2 + ((*(p+1) & -2) >> 1);
 	}
 	p+=2;
 	for(i = 0; i < mult; ++i){
@@ -79,32 +116,32 @@ void writeHeap(int targetBlock, char payload, int mult){
 
 
 void printHeap(int targetBlock, int mult){
-	char *p = head;						
-	char *end = (p+127);
+	unsigned char *p = head;						
+	unsigned char *end = (p+127);
 	int i;
-	printf("printheap :: targetblock: %d, mult: %d \n", targetBlock, mult);
+	//printf("printheap :: targetblock: %d, mult: %d \n", targetBlock, mult);
 	while((p < end) && (*p != targetBlock)){
-		printf(" block id: %d \n", *p);
-		p = p + 2 + (*(p+1) & -2);
+		//printf(" block id: %d \n", *p);
+		p = p + 2 + ((*(p+1) & -2)>>1);
 	}
 	p+=2;
 	for(i = 0; i < mult; ++i){
-		printf("i: %d %c ", i, *(p+i));
+		printf("%c ", *(p+i));
 	}
-	printf("\nend printheap\n");						
+	//printf("\nend printheap\n");						
 }
 
 
 void printHeader(int targetBlock){
-	char *p = head;						
-	char *end = (p+127);
+	unsigned char *p = head;						
+	unsigned char *end = (p+127);
 	int i;
-	printf("targetblock: %d \n", targetBlock);
+	//printf("targetblock: %d \n", targetBlock);
 	while((p < end) && (*p != targetBlock)){
-		printf("block id: %d \n", *p);
-		p = p + 2 + (*(p+1) & -2);
+		//printf("block id: %d \n", *p);
+		p = p + 2 + ((*(p+1) & -2) >> 1);
 	}
-	printf("%02x%02x\n", *p, *(p+1));
+	printf("%02x%02x\n", *p, (((*(p+1)&-2)>>1)+2) + (*(p+1)&1));
 	printf("\n");						
 }
 
@@ -122,27 +159,27 @@ void processResponse(char response[MAXINPUTSIZE]){
 	char payload;
 
 	sscanf(response, "%s", commandTitle);
-	printf("response: %s \n", response);
+	//printf("response: %s \n", response);
 	//count = sscanf(response, "%s %d %d %d %d", commandTitle, &args[0], &args[1], &args[2], &args[3]);
 	//printf("count %d\n", count);
 	if(!strcmp(commandTitle, "allocate")){
 	count = sscanf(response, "%s %d %^d %^d %^d", commandTitle, &args[0]);
 		if(count == 2){
-			printf("value of arg0: %d", args[0]);
+			//printf("value of arg0: %d\n", args[0]);
 			allocate(args[0]);
 		}
 		else{
-        		printf("Improper use of %s. %d arguments given, 1 expected...\n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
 		}
 	}					
 	else if(!strcmp(commandTitle, "free")){
 	count = sscanf(response, "%s %d %^d %^d %^d", commandTitle, &args[0]);
 		if(count == 2){
-			
+			freeBlock(args[0]);	
 		}
 		else{
-        		printf("Improper use of %s. %d arguments given, 1 expected...\n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
 		}
 	}				
@@ -152,18 +189,18 @@ void processResponse(char response[MAXINPUTSIZE]){
         		blockList();	
         	}
         	else{
-        		printf("Improper use of %s. %d arguments given, 0 expected...\n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
         	}
         }
 	else if(!strcmp(commandTitle, "writeheap")){
-	printf("response in writeheap: %s ...\n", response);
+	//printf("response in writeheap: %s ...\n", response);
 	count = sscanf(response, "%s %d %c %d", commandTitle, &args[0], &payload, &args[2]);
-	printf("%d \n", count); 
+	//printf("%d \n", count); 
 		if(count == 4){
        			writeHeap(args[0], payload, args[2]);
        		}
        		else{
-       			printf("Improper use of %s .%d arguments given, 3 expected... \n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
        		}
        	}			
@@ -173,7 +210,7 @@ void processResponse(char response[MAXINPUTSIZE]){
         		printHeap(args[0], args[1]);
         	}
         	else{
-       			printf("Improper use of %s .%d arguments given, 2 expected... \n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
         	}
         }
@@ -183,7 +220,7 @@ void processResponse(char response[MAXINPUTSIZE]){
         		printHeader(args[0]);	
         	}
         	else{
-       			printf("Improper use of %s .%d arguments given, 1 expected... \n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
         	}
         }			
@@ -194,7 +231,7 @@ void processResponse(char response[MAXINPUTSIZE]){
 			quit();	
        		}
        		else{
-       			printf("Improper use of %s .%d arguments given, 0 expected... \n", commandTitle, count);
+        		printf("Improper use of %s.\n", commandTitle);
 			return;
         	}			
 	}
@@ -211,10 +248,12 @@ int main(int argc, char* argv[]){
 	blockId = 1;
 	char response[MAXINPUTSIZE];
 	head = malloc(128);	
-	*head = blockId;		//first byte in head's header is its block number; we start at 1
-	*(head+1) = 126;		//second byte in head's heaer is its size in bytes
+	*head = 0;		//first byte in head's header is its block number; we start at 1
+	*(head+1) = (252);			//second byte in head's heaer is its size in bytes
+	//printf("Main:: *head: %d, allocated: %d,  *head+1: %d\n", *head, *(head+1)&1, *(head+1));	
 	
-	
+	//printf("Main:: *size: %d, allocated: %d,  *head+1: %d\n", (*(head+1)&-2)>>1, *(head+1)&1, *(head+1)&-2);
+	//printf("Main:: blockid: %d\n", blockId);
 	while(1){
 		printf("> ");
 		if(fgets(response, MAXINPUTSIZE, stdin)!=NULL){
